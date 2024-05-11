7f3d582172f41a8ba4232d59cf4154c803a7d37a
holybasil <holybasil.1128@gmail.com>
2024-05-11 17:25:25 +0800

    doc: badge api

--- examples/src/attest-server.js
+++ examples/src/attest-server.js
@@ -9,21 +9,37 @@ import { badges } from './badges.js';
 
 const app = express();
 const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT);
-const signer = (new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY)).connect(provider);
+const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY).connect(provider);
 
 // example query:
-// curl 'localhost:3000/api/badge/0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0/claim?recipient=0x0000000000000000000000000000000000000001'
-app.get('/api/badge/:address/claim', async (req, res) => {
-  const { recipient } = req.query;
-  const { address } = req.params;
+// curl 'localhost:3000/api/check?badge=0x30C98067517f8ee38e748A3aF63429974103Ea6B&recipient=0x0000000000000000000000000000000000000001'
+app.get('/api/check', async (req, res) => {
+  const { badge: badgeAddress, recipient } = req.query;
 
-  if (!recipient) return res.json({ error: 'missing query parameter "recipient"' });
-  if (!address) return res.json({ error: 'missing parameter "address"' });
+  if (!recipient) return res.json({ code: 0, message: 'missing query parameter "recipient"' });
+  if (!badgeAddress) return res.json({ code: 0, message: 'missing parameter "badge"' });
+  const badge = badges[normalizeAddress(badgeAddress)];
 
-  const badge = badges[normalizeAddress(address)];
+  if (!badge) return res.json({ code: 0, message: `unknown badge "${address}"` });
+  const eligibility = await badge.isEligible(recipient);
+  if (!eligibility) return res.json({ code: 0, message: 'why the recipient is not eligible', eligibility: false });
 
-  if (!badge) return res.json({ error: `unknown badge "${address}"` });
-  if (!badge.isEligible(recipient)) return res.json({ error: null, status: 'not eligible' });
+  res.json({ code: 1, message: 'success', eligibility: true });
+});
+
+// example query:
+// curl 'localhost:3000/api/claim?badge=0x30C98067517f8ee38e748A3aF63429974103Ea6B&recipient=0x0000000000000000000000000000000000000001'
+app.get('/api/claim', async (req, res) => {
+  const { badge: badgeAddress, recipient } = req.query;
+
+  if (!recipient) return res.json({ code: 0, message: 'missing query parameter "recipient"' });
+  if (!badgeAddress) return res.json({ code: 0, message: 'missing parameter "badge"' });
+
+  const badge = badges[normalizeAddress(badgeAddress)];
+
+  if (!badge) return res.json({ code: 0, message: `unknown badge "${badgeAddress}"` });
+  const eligibility = await badge.isEligible(recipient);
+  if (!eligibility) return res.json({ code: 0, message: 'not eligible' });
 
   const proxy = new EIP712Proxy(badge.proxy);
 
@@ -36,7 +52,7 @@ app.get('/api/badge/:address/claim', async (req, res) => {
   });
 
   const tx = await proxy.contract.attestByDelegation.populateTransaction(attestation);
-  res.json({ error: null, status: 'eligible', tx });
+  res.json({ code: 1, message: 'success', tx });
 });
 
 // Start the server
